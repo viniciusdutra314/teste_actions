19c5097bd841ae977133edacf0c850b2cb3651de
viniciusdutra314 <dutravinisousa2@usp.br>
2025-01-11 12:35:15 -0300

    poetr

--- tests/test_exemplo.py
+++ tests/test_exemplo.py
@@ -1,6 +1,6 @@
 import sys 
 from typing import NamedTuple, TypeVar, Generic
-
+import LabIFSC as lab
 def test_simples():
     assert 1 + 1 == 2 
 
@@ -13,3 +13,6 @@ def test_python_311():
 
     # A functional syntax is also supported
     Employee = NamedTuple('Employee', [('name', str), ('id', int)])
+def test_labifsc1():
+    x=lab.Medida(1,'m',1)
+    assert x.nominal==1
