6cafbf18c3eb7a84c48793d5d57b96cf30e992af
viniciusdutra314 <dutravinisousa2@usp.br>
2025-01-11 11:20:38 -0300

    depende de versao

--- tests/test_exemplo.py
+++ tests/test_exemplo.py
@@ -1,2 +1,8 @@
+import sys 
+
 def test_simples():
     assert 1 + 1 == 2 
+
+def test_dependente_de_versao():
+    if not sys.version_info.minor == 12:
+        raise ValueError("Versão inválida") 
