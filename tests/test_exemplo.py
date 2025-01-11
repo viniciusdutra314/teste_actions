1588121820c2236eecda19f81cbd4e330a6558f5
viniciusdutra314 <dutravinisousa2@usp.br>
2025-01-11 12:13:15 -0300

    teste

--- tests/test_exemplo.py
+++ tests/test_exemplo.py
@@ -1,8 +1,15 @@
 import sys 
+from typing import NamedTuple, TypeVar, Generic
 
 def test_simples():
     assert 1 + 1 == 2 
 
-def test_dependente_de_versao():
-    if not sys.version_info.minor == 12:
-        raise ValueError("Versão inválida") 
+def test_python_311():
+    T = TypeVar("T")
+
+    class Group(NamedTuple, Generic[T]):
+        key: T
+        group: list[T]
+
+    # A functional syntax is also supported
+    Employee = NamedTuple('Employee', [('name', str), ('id', int)])
